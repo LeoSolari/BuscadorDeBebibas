09a5ab4836f5887d94fa881d27a894fe774ae209
LeoSolari <95233443+LeoSolari@users.noreply.github.com>
2022-08-11 03:34:40 -0300

    Add files via upload

---
+++ src/components/Bebida.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Col, Card, Button} from 'react-bootstrap'
+import useBebidas from '../hooks/useBebidas'
+
+const Bebida = ({bebida}) => {
+
+    const {handleModalClick,handleBebidaIdClick} = useBebidas()
+
+  return (
+    <Col md={6} xl={3} >
+        <Card className='mb-4'>
+            <Card.Img
+                variant='top'
+                src={bebida.strDrinkThumb}
+                alt='Imagen de la bebida'
+            />
+        <Card.Body>
+            <Card.Title>
+                {bebida.strDrink}
+            </Card.Title>
+            <Button 
+                variant='warning'
+                className='w-100 text-uppercase mt-2'
+                onClick={() => {
+                    handleModalClick()
+                    handleBebidaIdClick(bebida.idDrink)
+                }}
+            >
+                Ver Receta
+            </Button>
+        </Card.Body>
+        </Card>
+    </Col>
+  )
+}
+
+export default Bebida
---
+++ src/components/ListadoBebidas.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Row } from 'react-bootstrap'
+import useBebidas from '../hooks/useBebidas'
+import Bebida from './Bebida'
+
+const ListadoBebidas = () => {
+
+    const {bebidas} = useBebidas()
+
+  return (
+    <Row>
+        {bebidas.map(bebida => (
+            <Bebida 
+                bebida={bebida}
+                key={bebida.idDrink}
+            />
+        ))}
+    </Row>
+  )
+}
+
+export default ListadoBebidas
